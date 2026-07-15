cb2a51a1c7a6bfd0373c9f72e77c090e136f5b13
Nikhil Verma <nikhilverma2918@gmail.com>
2026-07-15 20:23:25 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (63.70%) - LeetSync

---
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head == NULL){
+            return head;
+        }
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evennode = even;
+        while(even && even->next){
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = evennode;
+        return head;
+    }
+};
