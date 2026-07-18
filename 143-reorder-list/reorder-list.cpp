92c45eba54ad6eae650ede81d2e3a8a6f0fd72e9
Nikhil Verma <nikhilverma2918@gmail.com>
2026-07-19 02:39:29 +0530

    Time: 1 ms (14.51%) | Memory: 23.9 MB (12.70%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -1,6 +1,86 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
+        ListNode* current = head;
+        vector<ListNode*> vec;
+        if(head == NULL){
+            return;
+        }
+        while(current){
+            vec.push_back(current);
+            current = current->next;
+        }
+        int n = vec.size();
+        int left = 0;
+        int right = n-1;
+        while(left<right){
+            vec[left]->next = vec[right];
+            left++;
+            vec[right]->next = vec[left];
+            right--;
+        }
+        vec[left]->next = NULL;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+class Solution {
+public:
+    void reorderList(ListNode* head) {
         if(head==NULL){
             return;
         }
@@ -22,3 +102,4 @@ public:
         vec[left]->next = NULL;
     }
 };
+*/
