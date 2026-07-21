0cf484a2053a06f6025a3bfe3f0dfca7343d9f1b
Nikhil Verma <nikhilverma2918@gmail.com>
2026-07-22 03:44:40 +0530

    Time: 0 ms (100.00%) | Memory: 77.1 MB (75.69%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -1,59 +1,47 @@
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-
-        ListNode* dummy = new ListNode(0);
-        ListNode* tail = dummy;
-
-        int carry = 0;
-
-        while (l1 != nullptr || l2 != nullptr || carry != 0) {
-
-            int sum = carry;
-
-            if (l1 != nullptr) {
-                sum += l1->val;
-                l1 = l1->next;
-            }
-
-            if (l2 != nullptr) {
-                sum += l2->val;
-                l2 = l2->next;
-            }
-
-            carry = sum / 10;
-
-            tail->next = new ListNode(sum % 10);
-            tail = tail->next;
+    ListNode* addTwoNumbers(ListNode* h1, ListNode* h2) {
+        if(h1==NULL && h2==NULL){
+            return NULL;
         }
 
+        ListNode* c1 = h1;
+        ListNode* c2 = h2;
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy;
+        int carry = 0;
+        while(c1 || c2){
+            int value1 = 0;
+            int value2 = 0;
+            if(c1){
+                value1 = c1->val;
+            }
+            if(c2){
+                value2 = c2->val;
+            }
+            int sum = value1+value2+carry;
+            if(sum>=10){
+                int ans = sum%10;
+                carry = sum/10;
+                tail->next = new ListNode(ans);
+                tail = tail->next;
+            }
+            else{
+                tail->next = new ListNode(sum);
+                tail = tail->next;
+                carry = 0;
+            }
+            if(c1){
+                c1 = c1->next;
+            }
+            if(c2){
+                c2 = c2->next;
+            }
+            
+        }
+        if (carry) {
+            tail->next = new ListNode(carry);
+        }
         return dummy->next;
     }
 };
-// class Solution {
-// public:
-//     ListNode* addTwoNumbers(ListNode* h1, ListNode* h2) {
-//         if(h1==NULL && h2 ==NULL){
-//             return NULL;
-//         }
-//         ListNode* c1 = h1;
-//         ListNode* c2 = h2;
-//         int x=0;
-//         while(c1 && c2){
-//             int sum = c1->val+c2->val+x;
-//             if((sum)>=10){
-//                 int ans = sum%10;
-//                 int x = sum/10;
-//                 ListNode* newhead = new ListNode(ans);
-//             }
-//             else{
-//                 ListNode* newhead = new ListNode(sum);
-//             }
-//             ListNode* tail = newhead;
-//             tail->next = new ListNode();
-//             tail = tail->next;
-//         }
-//     }
-// };
-
-//i know that the placement of some of my lines are wrong, like the placement of tail node, and i tink even its initialosation is wrong. and still there are some lofical errors in the code coz the code is not behaving according to the logic i proposed.
